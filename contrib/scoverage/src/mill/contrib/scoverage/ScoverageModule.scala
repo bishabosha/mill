2610c801dfa7121533f2d89707f1f33cc65f694f
Jamie Thompson <bishbashboshjt@gmail.com>
2024-09-16 20:45:27 +0200

    Part 12 - rework contrib.scoverage.api to be java based.

--- contrib/scoverage/src/mill/contrib/scoverage/ScoverageModule.scala
+++ contrib/scoverage/src/mill/contrib/scoverage/ScoverageModule.scala
@@ -3,7 +3,7 @@ package mill.contrib.scoverage
 import coursier.Repository
 import mill._
 import mill.api.{Loose, PathRef, Result}
-import mill.contrib.scoverage.api.ScoverageReportWorkerApi.ReportType
+import mill.contrib.scoverage.api.ScoverageReportWorkerApi2.ReportType
 import mill.main.BuildInfo
 import mill.scalalib.api.ZincWorkerUtil
 import mill.scalalib.{Dep, DepSyntax, JavaModule, ScalaModule}
@@ -126,7 +126,8 @@ trait ScoverageModule extends ScalaModule { outer: ScalaModule =>
     millProjectModule(
       workerArtifact,
       repositoriesTask(),
-      resolveFilter = _.toString.contains(workerArtifact)
+      resolveFilter = _.toString.contains(workerArtifact),
+      artifactSuffix = if (isScov2) "_3" else "_2.13"
     )
   }
 
